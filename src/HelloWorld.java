278fa9d48b4e942a80a2bd1312eaf36826079107
User <User@LAPTOP-HEUAOA06>
2020-03-19 22:20:17 +0900

    Create a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println(" This is your studying planner ! ");
+		System.out.println("1. Add assignment");
+		System.out.println("2. Delete assignment");
+		System.out.println("3. Edit assignment");
+		System.out.println("4. View assignment");
+		System.out.println("5. Time table");
+		System.out.println("6. Test scheduel");
+		System.out.println("7. Exit");
+	}
+
+}
